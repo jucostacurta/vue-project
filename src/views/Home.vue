03d204847fea198c3d7a3aedb54df8738503e8dd
Juliana Costacurta <juliana.costacurta@consultores.rnp.br>
2023-12-26 17:46:51 -0300

    feat/add-searching-feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,33 +5,84 @@ import API from "./service/api";
 
 import Card from "@/components/Card.vue";
 
-const fetchedData = ref([]);
+const fetchedInitialData = ref([]);
+const term = ref("");
+const isLoading = ref(false);
 
-onMounted(() => {
-  axios
-    .get(API.GET_ALL_COUNTRIES)
-    .then((response) => {
-      const sortedData = response.data.sort((a, b) =>
-        a.name.common > b.name.common
-          ? 1
-          : b.name.common > a.name.common
-          ? -1
-          : 0
-      );
-      fetchedData.value = sortedData;
-    })
-    .catch((error) => {
-      console.log(error);
+const sortBy = (data) =>
+  data.sort((a, b) =>
+    a.name.common > b.name.common ? 1 : b.name.common > a.name.common ? -1 : 0
+  );
+
+onMounted(async () => {
+  isLoading.value = true;
+  try {
+    await axios.get(API.GET_ALL_COUNTRIES).then((response) => {
+      const sortedData = sortBy(response.data);
+      fetchedInitialData.value = sortedData;
     });
-
-  return { fetchedData };
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
 });
+
+const searchByName = async () => {
+  isLoading.value = true;
+  try {
+    await axios
+      .get(`${API.GET_BY_COUNTRY_NAME}/${term.value}`)
+      .then((response) => {
+        const sortedData = sortBy(response.data);
+        fetchedInitialData.value = sortedData;
+      });
+  } catch (error) {
+    fetchedInitialData.value = [];
+
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
 
 <template>
-  <v-container>
+  <v-container v-if="isLoading">
     <v-row align="center" justify="center">
-      <Card v-for="item in fetchedData" v-bind:key="item" :item="item" />
+      <v-progress-circular
+        :size="50"
+        color="primary"
+        class="mt-5"
+        indeterminate
+      />
+    </v-row>
+  </v-container>
+
+  <v-container v-else>
+    <v-row align="center" justify="center">
+      <v-col cols="10" class="mt-2">
+        <v-text-field
+          :append-icon="term ? 'mdi-filter-check' : 'mdi-filter'"
+          :append-inner-icon="'mdi-magnify'"
+          v-model="term"
+          variant="outlined"
+          clear-icon="mdi-close-circle"
+          clearable
+          label="Busque pelo nome do país"
+          type="text"
+          @click:append="console.log('open filters')"
+          @click:append-inner="searchByName"
+          @keydown.enter="searchByName"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <Card v-for="item in fetchedInitialData" v-bind:key="item" :item="item" />
+    </v-row>
+    <v-row align="center" justify="center" v-if="fetchedInitialData.length < 1">
+      <v-col cols="10" class="mt-2"> Nenhum resultado encontrado. </v-col>
     </v-row>
   </v-container>
 </template>
--- src/views/service/api.js
+++ src/views/service/api.js
@@ -1,4 +1,5 @@
 export const API = {
   GET_ALL_COUNTRIES: "https://restcountries.com/v3.1/all",
+  GET_BY_COUNTRY_NAME: "https://restcountries.com/v3.1/name",
 };
 export default API;
