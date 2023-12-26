29e36c906a57ace0ad30d42ee7240ba55283897b
Juliana Costacurta <juliana.costacurta@consultores.rnp.br>
2023-12-26 10:56:29 -0300

    feat: removing hello world and adding toolbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
+  <div>
+    <v-toolbar color="deep-purple">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      Base Preset
-    </v-app-bar-title>
-  </v-app-bar>
+      <v-toolbar-title>Testing Vue with Vuetify</v-toolbar-title>
+    </v-toolbar>
+  </div>
 </template>
 
 <script setup>
-  //
+//
 </script>
