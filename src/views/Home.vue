71af0834de5ec6c19a6ecd2b3a8fb383a96e31f1
Juliana Costacurta <juliana.costacurta@consultores.rnp.br>
2023-12-27 14:23:49 -0300

    feat: add filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,15 @@ import Card from "@/components/Card.vue";
 const fetchedInitialData = ref([]);
 const term = ref("");
 const isLoading = ref(false);
+const dialog = ref(false);
+const continents = ref("All");
 
 const sortBy = (data) =>
   data.sort((a, b) =>
     a.name.common > b.name.common ? 1 : b.name.common > a.name.common ? -1 : 0
   );
 
-onMounted(async () => {
+const searchAll = async () => {
   isLoading.value = true;
   try {
     await axios.get(API.GET_ALL_COUNTRIES).then((response) => {
@@ -26,10 +28,11 @@ onMounted(async () => {
   } finally {
     isLoading.value = false;
   }
-});
+};
 
 const searchByName = async () => {
   isLoading.value = true;
+  if (!term.value) return searchAll();
   try {
     await axios
       .get(`${API.GET_BY_COUNTRY_NAME}/${term.value}`)
@@ -39,12 +42,33 @@ const searchByName = async () => {
       });
   } catch (error) {
     fetchedInitialData.value = [];
-
     console.log(error);
   } finally {
     isLoading.value = false;
   }
 };
+
+const searchByContinent = async () => {
+  isLoading.value = true;
+  if (!continents.value || continents.value === "All") return searchAll();
+  try {
+    await axios
+      .get(`${API.GET_BY_CONTINENT}/${continents.value}`)
+      .then((response) => {
+        const sortedData = sortBy(response.data);
+        fetchedInitialData.value = sortedData;
+      });
+  } catch (error) {
+    fetchedInitialData.value = [];
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(async () => {
+  searchAll();
+});
 </script>
 
 <template>
@@ -63,7 +87,9 @@ const searchByName = async () => {
     <v-row align="center" justify="center">
       <v-col cols="10" class="mt-2">
         <v-text-field
-          :append-icon="term ? 'mdi-filter-check' : 'mdi-filter'"
+          :append-icon="
+            term || continents !== 'All' ? 'mdi-filter-check' : 'mdi-filter'
+          "
           :append-inner-icon="'mdi-magnify'"
           v-model="term"
           variant="outlined"
@@ -71,18 +97,93 @@ const searchByName = async () => {
           clearable
           label="Busque pelo nome do país"
           type="text"
-          @click:append="console.log('open filters')"
+          @click:append="dialog = true"
           @click:append-inner="searchByName"
           @keydown.enter="searchByName"
-        ></v-text-field>
+        />
       </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialog"
+        transition="dialog-bottom-transition"
+        width="500"
+      >
+        <v-card>
+          <v-toolbar dark color="primary">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-toolbar-title>Filtro</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+              <v-btn
+                variant="text"
+                @click="
+                  dialog = false;
+                  searchByContinent();
+                "
+              >
+                Salvar
+              </v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+          <v-radio-group v-model="continents">
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-radio color="primary" label="All" value="All" />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio color="primary" label="Asia" value="Asia" />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio color="primary" label="Africa" value="Africa" />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio
+                    color="primary"
+                    label="North America"
+                    value="North America"
+                  />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio
+                    color="primary"
+                    label="South America"
+                    value="South America"
+                  />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio
+                    color="primary"
+                    label="Antarctica"
+                    value="Antarctica"
+                  />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio color="primary" label="Europe" value="Europe" />
+                </v-col>
+                <v-col cols="6">
+                  <v-radio
+                    color="primary"
+                    label="Australia"
+                    value="Australia"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-radio-group>
+        </v-card>
+      </v-dialog>
     </v-row>
 
     <v-row align="center" justify="center">
       <Card v-for="item in fetchedInitialData" v-bind:key="item" :item="item" />
     </v-row>
     <v-row align="center" justify="center" v-if="fetchedInitialData.length < 1">
-      <v-col cols="10" class="mt-2"> Nenhum resultado encontrado. </v-col>
+      <v-col cols="10" class="mt-2">Nenhum resultado encontrado.</v-col>
     </v-row>
   </v-container>
 </template>
